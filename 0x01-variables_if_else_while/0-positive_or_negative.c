8652e2f0366e0cd58f1d29007e5a0da8b5865ce5
JackTravisKyeyune <juliesenynam@gmail.com>
2022-09-09 09:39:22 -0700

    updated

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints a random number and it states whether it is positive, negative, or zero
+ * main - Prints a number whether it is positive, negative, or zero
  *
  * Return: Always 0 (Success)
  */
