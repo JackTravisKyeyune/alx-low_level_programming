ccc5ed0b4582bea00c59340acf94fdefcddf09ca
JackTravisKyeyune <juliesenynam@gmail.com>
2022-09-10 04:01:48 -0700

    more combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,6 +23,12 @@ while (d < 10)
 c = 0;
 while (c < 10)
 {
+if (!(f == c && e == d))
+putchar('0' + f);
+putchar('0' + e);
+putchar(' ');
+putchar('0' + d);
+putchar('0' + c);
 if (!(f + e == 18 && c + d == 17 && d == 9))
 {
 putchar(',');
