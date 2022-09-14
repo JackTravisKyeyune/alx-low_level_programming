3434bd3db819096e8ca3d7905ca73f7f952591a0
JackTravisKyeyune <juliesenynam@gmail.com>
2022-09-13 18:49:43 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_10x - print alphabet 10x folowed by new line
+ *
+ * Return: Always 0
+ */
+ void print_alphabet_x10(void)
+ {
+	 char n;
+	 char i;
+	 for (i = 10; i <= 10; i++)
+ {
+	 for (n = 'a'; n <= 'z'; n++)
+	 {
+	 _putchar(n);
+	 _putchar('\n');
+	 }
+ }
+ }
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isapha - show 1 if the input is c
+ * letter another cases, shows 0
+ * @c: Character to check
+ *
+ * Returns: 1 for letters, 0 for otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || ((c >= 'A' && c <= 'Z'));
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
