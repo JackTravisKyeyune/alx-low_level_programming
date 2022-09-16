84564d7918c1fac42f7cbc5b6f048a826848a832
JackTravisKyeyune <juliesenynam@gmail.com>
2022-09-16 03:55:02 -0700

    more tasks of functions nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,24 +7,25 @@
  */
 void print_diagonal(int n)
 {
-	int i, j;
-
 	if (n <= 0)
-		_putchar('\n');
-
-	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j <= 1; j++)
+		_putchar('\n');
+		}
+	else
+	{
+
+		int i, j;
+
+		for (i = 0; j < n; j++)
 		{
-			if (i == j)
+			for (j = 0; j < n; j++)
 			{
-				_putchar('\\');
-				_putchar('\n');
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
 			}
-			else
-			{
-				_putchar(' ');
-			}
+			_putchar('\n');
 		}
 	}
 }
