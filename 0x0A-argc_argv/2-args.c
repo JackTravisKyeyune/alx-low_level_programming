921faf57487dd00954a76b837dc79db8e9adf35e
juliesenynam@gmail.com <juliesenynam@gmail.com>
2022-09-30 12:29:38 -0700

    program that multiplies two numbers and receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * argc: nmber of command line arguments
+ * @argv: array that contains the command line arguments of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
