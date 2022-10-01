709da460f18f1cfeeadd816f73b1df7fd9896231
juliesenynam@gmail.com <juliesenynam@gmail.com>
2022-10-01 04:31:57 -0700

    static library

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - Returns absolute value ofgiven number
+ * @n: input number as an integer
+ * Return: Absolute value of n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+}
